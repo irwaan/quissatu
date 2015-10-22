40c254174768c09f08bfcc0c3b0505bf18d5aca5
irwaan <eyiwulandari88@yahoo.co.id>
2015-10-22 22:54:03 +0700

    versi 1.0

---
+++ src/newpackage/lingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author acerOne14
+ */
+public class lingkaran {
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public double Lingkaran (double a){
+        double luas = (a*a*3.14);
+        return luas;
+    }
+    
+    
+}
---
+++ src/newpackage/persegi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author acerOne14
+ */
+public class persegi {
+    public int Lpersegi (int a){
+        int luas = (int)(a*a);
+        return luas;
+    }
+
+    public int Lersegi(int a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
